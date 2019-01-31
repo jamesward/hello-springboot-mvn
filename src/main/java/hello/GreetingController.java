b533c80ea07191c085db789d88b7e2e7614528ca
James Ward <james@jamesward.com>
2019-01-31 10:18:40 -0700

    more accurate message

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-  private static final String TEMPLATE = "Hello, %s! You are visitor number %s";
+  private static final String TEMPLATE = "Hello, %s! You are visitor number %s on this instance since it was started";
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/")
