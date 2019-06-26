68c60f2ea367d89cbb042e89d28af3bf211ed4c0
James Ward <james@jamesward.com>
2019-06-26 08:15:09 -0600

    cleanup

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,13 +2,26 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-/** Application example. */
 @SpringBootApplication
+@RestController
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+
+  @RequestMapping("/")
+  public String index() {
+    String revision = System.getenv("K_REVISION");
+    if (revision == null) {
+      revision = "(unknown version)";
+    }
+
+    return String.format("hello, world - %s", revision);
+  }
+
 }
 
