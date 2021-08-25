7d61e7de535edcfcd6180fa948786a66c7564357
James Ward <james@jamesward.com>
2021-08-25 16:11:53 -0600

    schedule me

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Hello Spring Boot
 -----------------
 
 [![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
+
+Run Locally:
+```
+./mvnw spring-boot:run
+```
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,13 +1,20 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableScheduling
 @RestController
 public class Application {
+
+  Logger logger = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
@@ -21,6 +28,11 @@ public class Application {
     }
 
     return String.format("hello, world - %s", revision);
+  }
+
+  @Scheduled(fixedRate = 5000)
+  public void reportCurrentTime() {
+    logger.info("The time is now " + System.currentTimeMillis());
   }
 
 }
